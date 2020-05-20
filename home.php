ffe487ce93c060599bb46a1d0858831224957e55
Rogger Marlon <roggermarlon@hotmail.com>
2020-05-20 04:59:20 -0300

    Correção das imagens

--- home.php
+++ home.php
@@ -42,12 +42,12 @@
               <div class="row">
                 <div class="col-6 d-flex justify-content-center">
                   <a href="abrir_chamado.php">
-                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                  <img src="abrir_chamado.png" width="70" height="70">
                   </a>                  
                 </div>
                 <div class="col-6 d-flex justify-content-center">
                   <a href="consultar_chamado.php">
-                  <img src="formulario_consultar_chamado.png" width="70" height="70">                                    
+                  <img src="consultar_chamado.png" width="70" height="70">                                    
                   </a>                  
                 </div>                
               </div>
